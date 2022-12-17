bfec5338e7fddab29d141f749a7b7dd2aa95e03f
HenryQinUp <51472137+HenryQinUp@users.noreply.github.com>
2022-12-17 18:01:20 +0800

    1

--- jk.py
+++ jk.py
@@ -48,7 +48,7 @@ lunxun='{\n            "name": "轮询",\n            "type": 2,\n            "u
 data4=re.sub('"parses": [\n', '"parses": [\n'+lunxun, data3, count=1,flags=re.S|re.I)
 ##添加web和json聚合解析
 juhe='{\n		"name": "Json聚合",\n		"type": 3,\n		"url": "Demo"\n	},\n {\n		"name": "Web聚合",\n		"type": 3,\n		"url": "Web"\n	},\n'
-data4=re.sub('"parses": [\n', '"parses": [\n'+juhe, data4, count=1,flags=re.S|re.I)
+data5=re.sub('"parses": [\n', '"parses": [\n'+juhe, data4, count=1,flags=re.S|re.I)
 # 设置 DNS 经过代理
 
 #data5=re.sub('external-controller: :9090\ndns:\n  enabled: true\n  nameserver:\n    - 119.29.29.29\n    - 223.5.5.5\n  fallback:\n    - 8.8.8.8\n    - 8.8.4.4\n    - tls://1.0.0.1:853\n    - tls://dns.google:853\n', "external-controller: :9090\ntcp-concurrent: true\nipv6: false\ndns:\n  enabled: true\n  ipv6: false \n  listen: 0.0.0.0:53\n  default-nameserver:\n    - 223.5.5.5\n    - 119.29.29.29\n    - 1.1.1.1\n  enhanced-mode: fake-ip\n  fake-ip-range: 198.18.0.1/16 \n  use-hosts: true \n  fake-ip-filter:\n    - '*.lan'\n    - localhost.ptlogin2.qq.com\n  nameserver:\n    - 'https://223.5.5.5/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://doh.pub/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://doh.360.cn/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://dns.alidns.com/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n  fallback:\n    - 'https://dns.google/dns-query#\u2601 国外故障转移'\n    - 'https://1.1.1.1/dns-query#\u2601 国外故障转移'\n    - 'https://cloudflare-dns.com/dns-query#\u2601 国外故障转移'\n    - 'https://doh.opendns.com/dns-query#\u2601 国外故障转移'\n  fallback-filter:\n    geoip: true\n    geoip-code: CN\n    ipcidr:\n      - 240.0.0.0/4\n", data4, count=1, flags=0)
@@ -56,4 +56,4 @@ data4=re.sub('"parses": [\n', '"parses": [\n'+juhe, data4, count=1,flags=re.S|re
 localtime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 with open("Q2WForever.json", "w",encoding='UTF-8') as out_file:
-    out_file.write('// https://henryqinup.github.io/JK/Q2WForever.json '+localtime+'\n'+data4)
+    out_file.write('// https://henryqinup.github.io/JK/Q2WForever.json '+localtime+'\n'+data5)
