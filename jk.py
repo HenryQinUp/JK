40714415299f54ea92190ebe0abe364adace322c
HenryQinUp <51472137+HenryQinUp@users.noreply.github.com>
2022-12-17 18:00:11 +0800

    1

--- jk.py
+++ jk.py
@@ -23,7 +23,7 @@ conten2 = requests.get(jarurl)  # 读取网页内容
 conten3=conten2.content
 h.write(conten3)
 h.close()
-    # 计算jar文件MD5值
+## 计算jar文件MD5值
 with open('HenryQin.jar', 'rb') as i:
     m = hashlib.md5(i.read()).hexdigest()
 
@@ -32,18 +32,23 @@ with open('HenryQin.jar', 'rb') as i:
 data1=re.sub('"spider":.*",', '"spider":"https://henryqinup.github.io/JK/HenryQin.jar;md5;'+m+'",', data, count=1, flags=re.M)
 # wallpaper替换
 data2=re.sub('"wallpaper": "http://101.34.67.237/pics",', '"wallpaper":"https://henryqinup.github.io/JK/background.jpg",', data1, count=1, flags=re.M)
-# 提取需要的站点，并且更改站点顺序
-
+# 提取需要的站点
 sitelibvio=re.compile('({\n            "key": "dr_LIBVIO",\n.*?},\n)',flags=re.S|re.I).search(data).group()
 sitebuka=re.compile('({\n            "key": "dr_真不卡",\n.*?},\n)',flags=re.S|re.I).search(data).group()
 
+## 更改站点顺序
 add=sitelibvio+sitebuka
-data3=re.sub('"sites":.\[\n', '"sites": [\n'+'    '+add,data2, count=1, flags=re.M|re.I)
+data3=re.sub('"sites":.\[\n', '"sites": [\n'+'    '+add,data2, count=1,flags=re.S|re.I)
 with open("jarurl.txt", "a+",encoding='UTF-8') as out_file:
     out_file.write('\n'+add)
 
 # 解析设置轮询并发优先
-#data4=re.sub('strategy: consistent-hashing', 'strategy: round-robin', data3, count=1, flags=0)
+lunxun='{\n            "name": "轮询",\n            "type": 2,\n            "url": "Sequence",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n        {\n            "name": "并发",\n            "type": 2,\n            "url": "Parallel",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n'
+#parses=re.compile('({\n            "key": "dr_LIBVIO",\n.*?},\n)',flags=re.S|re.I).search(data).group()
+data4=re.sub('"parses": [\n', '"parses": [\n'+lunxun, data3, count=1,flags=re.S|re.I)
+##添加web和json聚合解析
+juhe='{\n		"name": "Json聚合",\n		"type": 3,\n		"url": "Demo"\n	},\n {\n		"name": "Web聚合",\n		"type": 3,\n		"url": "Web"\n	},\n'
+data4=re.sub('"parses": [\n', '"parses": [\n'+juhe, data4, count=1,flags=re.S|re.I)
 # 设置 DNS 经过代理
 
 #data5=re.sub('external-controller: :9090\ndns:\n  enabled: true\n  nameserver:\n    - 119.29.29.29\n    - 223.5.5.5\n  fallback:\n    - 8.8.8.8\n    - 8.8.4.4\n    - tls://1.0.0.1:853\n    - tls://dns.google:853\n', "external-controller: :9090\ntcp-concurrent: true\nipv6: false\ndns:\n  enabled: true\n  ipv6: false \n  listen: 0.0.0.0:53\n  default-nameserver:\n    - 223.5.5.5\n    - 119.29.29.29\n    - 1.1.1.1\n  enhanced-mode: fake-ip\n  fake-ip-range: 198.18.0.1/16 \n  use-hosts: true \n  fake-ip-filter:\n    - '*.lan'\n    - localhost.ptlogin2.qq.com\n  nameserver:\n    - 'https://223.5.5.5/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://doh.pub/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://doh.360.cn/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n    - 'https://dns.alidns.com/dns-query#\U0001F1E8\U0001F1F3 国内故障转移'\n  fallback:\n    - 'https://dns.google/dns-query#\u2601 国外故障转移'\n    - 'https://1.1.1.1/dns-query#\u2601 国外故障转移'\n    - 'https://cloudflare-dns.com/dns-query#\u2601 国外故障转移'\n    - 'https://doh.opendns.com/dns-query#\u2601 国外故障转移'\n  fallback-filter:\n    geoip: true\n    geoip-code: CN\n    ipcidr:\n      - 240.0.0.0/4\n", data4, count=1, flags=0)
@@ -51,4 +56,4 @@ with open("jarurl.txt", "a+",encoding='UTF-8') as out_file:
 localtime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 with open("Q2WForever.json", "w",encoding='UTF-8') as out_file:
-    out_file.write('// https://henryqinup.github.io/JK/Q2WForever.json '+localtime+'\n'+data3)
+    out_file.write('// https://henryqinup.github.io/JK/Q2WForever.json '+localtime+'\n'+data4)
