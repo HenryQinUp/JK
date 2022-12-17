b1c133a4bd6c5528d73ad2398a6f34017ca053ac
HenryQinUp <51472137+HenryQinUp@users.noreply.github.com>
2022-12-17 18:26:30 +0800

    1

--- jk.py
+++ jk.py
@@ -45,7 +45,7 @@ with open("jarurl.txt", "a+",encoding='UTF-8') as out_file:
 # 解析设置轮询并发优先
 lunxun='{\n		"name": "Json\u805a\u5408",\n		"type": 3,\n		"url": "Demo"\n	},\n {\n		"name": "Web\u805a\u5408",\n		"type": 3,\n		"url": "Web"\n	},\n'
 #parses=re.compile('({\n            "key": "dr_LIBVIO",\n.*?},\n)',flags=re.S|re.I).search(data).group()
-data4=re.sub('"parses": [\n','"parses": [\n'+lunxun, data3, count=1,flags0)
+data4=re.sub('"parses": [\n','"parses": [\n'+lunxun, data3, count=1,flags=0)
 ##添加web和json聚合解析
 juhe='{\n		"name": "Json\u805a\u5408",\n		"type": 3,\n		"url": "Demo"\n	},\n {\n		"name": "Web\u805a\u5408",\n		"type": 3,\n		"url": "Web"\n	},\n'
 data5=re.sub('"parses": [\n', '"parses": [\n'+juhe, data4, count=1,flags=re.S|re.I)
