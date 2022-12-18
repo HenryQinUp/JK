2e8ef09186b410a7c953bda8fbb99951514a4c5e
HenryQinUp <51472137+HenryQinUp@users.noreply.github.com>
2022-12-18 14:14:21 +0800

    1

--- jk.py
+++ jk.py
@@ -43,7 +43,7 @@ with open("jarurl.txt", "a+",encoding='UTF-8') as out_file:
     out_file.write('\n'+add)
 
 # 解析设置轮询并发优先
-lunxun=re.compile('{\n            "name": "轮询",\n            "type": 2,\n            "url": "Sequence",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n        {\n            "name": "并发",\n            "type": 2,\n            "url": "Parallel",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n',flags=re.S|re.I).search(data).group()
+lunxun=re.compile('({\n            "name": "轮询",\n            "type": 2,\n            "url": "Sequence",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n        {\n            "name": "并发",\n            "type": 2,\n            "url": "Parallel",\n            "header": {\n                "User-Agent": "Mozilla/5.0"\n            }\n        },\n)',flags=re.S|re.I).search(data).group()
 #parses=re.compile('({\n            "key": "dr_LIBVIO",\n.*?},\n)',flags=re.S|re.I).search(data).group()
 data4=re.sub('"parses": [\n','"parses": [\n'+lunxun, data3, count=1,flags=0)
 ##添加web和json聚合解析
